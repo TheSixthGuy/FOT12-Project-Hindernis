2d30fbe54716016188fcfc00a1881b4fee01b8fb
Till Finke <35559717+TheSixthGuy@users.noreply.github.com>
2019-03-18 10:49:21 +0100

    Add GreifenTest-Funktion
    A programm for testing the Servo Motors we get.

---
+++ GreifenTest-Funktion.py
@@ -0,0 +1,30 @@
+#Greifen-Funktion
+ import RPi.GPIO as GPIO
+ import time
+ 
+GPIO.setwarnings(False)  #Zum ausschalten von nicht kritischen fehlermeldungen
+
+GPIO.setmode(GPIO.BCM)   #Einschalten des Boardmode(Pin-nummerierung)
+print("Boardmode set")
+
+MotorC = 5
+
+GPIO.setup(MotorC, GPIO.OUT)  #Pins als Ausgange definieren
+#GPIO.setup(MotorB, GPIO.OUT)  #
+
+def Grip():         #Funktion um die gewollte Drehung der Motoren auszuführen
+    duty = 90 / 18 + 2          #Benötiger Arbeitszyklus ausrechnen
+    GPIO.output(MotorC, GPIO.HIGH)  #Ausgabe auf den PIN von MotorA
+    pwm.ChangeDutyCycle(duty)       #Ändern des Arbeitszyklus auf den ausgerechneten Wert
+    #time.sleep(1)                   #1 Sekunde warten
+   
+	
+def Release():
+	GPIO.output(MotorC,GPIO.LOW)    #Ausgabe auf den PIN von MotorA beenden
+	pwm.ChangeDutyCycle(0)
+	duty = -90 / 18 + 2
+	GPIO.output(MotorC, GPIO.HIGH)
+	pwm.ChangeDutyCycle(duty)       #Ändern des Arbeitszyklus auf den ausgerechneten Wert
+	time.sleep(1)
+	GPIO.output(MotorC, GPIO.LOW)
+	pwm.ChangeDutyCycle(0)
