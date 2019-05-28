f8019699b1d0d652e9cb0ebc7ccfe3d9685dd232
Till Finke <35559717+TheSixthGuy@users.noreply.github.com>
2019-05-28 08:37:03 +0200

    website mit reset func

---
+++ reset.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="start.php" method="GET">
+    <button name="reset" label="Neustart>Neustart</button>
+</form>
+<?php
+    system('python main.py')
+?>
+</body>
+</html>
